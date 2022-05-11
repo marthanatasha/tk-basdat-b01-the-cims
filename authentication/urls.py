3f97bbf593946fe93093a9a7e08d9bd093f7b7b5
Adillah Putri <adllhputri@gmail.com>
2022-05-11 13:21:51 +0000

    Implement login

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import login, register_admin, register_pemain
+from .views import login, register_admin, register_pemain, homepage, logout
 
 urlpatterns = [
+	path('', homepage, name=''),
 	path('login', login, name='login'),
+	path('logout', logout, name='logout'),
 	path('register/admin', register_admin, name='register_admin'),
 	path('register/pemain', register_pemain, name='register_pemain'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,11 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db import connection
 
+def homepage(request):
+    try:
+        role = request.session["role"]
+    except:
+        return redirect("/login")
+
+    args = {}
+    if role == "admin":
+        args["role"] = "admin"
+        args["username"] = request.session["username"]
+    else:
+        args["role"] = "pemain"
+        args["username"] = request.session["username"]
+        args["email"] = request.session["email"]
+        args["no_hp"] = request.session["no_hp"]
+        args["koin"] = request.session["koin"]
+    return render(request, "homepage.html", args)
+
 def	login(request):
-  return render(request, "login.html")
+    if request.method == "POST":
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM ADMIN WHERE username='{}' AND password='{}'".format(request.POST["username"], request.POST["password"]))
+            row = cursor.fetchall()
+            if len(row) > 0:
+                request.session["username"] = row[0][0]
+                request.session["role"] = "admin"
+                return redirect("/")
+            else:
+                cursor.execute("SELECT * FROM PEMAIN WHERE username='{}' AND password='{}'".format(request.POST["username"], request.POST["password"]))
+                row = cursor.fetchall()
+                if len(row) > 0:
+                    request.session["username"] = row[0][0]
+                    request.session["email"] = row[0][1]
+                    request.session["no_hp"] = row[0][3]
+                    request.session["koin"] = row[0][4]
+                    request.session["role"] = "pemain"
+                    return redirect("/")
+                else:
+                    return render(request, "login.html", {"error":"Username atau password salah"})
+
+    return render(request, "login.html", {"error":""})
+
+def logout(request):
+    request.session.flush()
+    return redirect("/login")
 
 def register_admin(request):
-  return render(request, "register_admin.html")
+    return render(request, "register_admin.html")
 
 def register_pemain(request):
-  return render(request, "register_pemain.html")
+    return render(request, "register_pemain.html")
