439c9f7e083684f8598a324d72aa524b87e8006b
Sulthan Muhammad Fasda <sulthan.muhammad01@ui.ac.id>
2022-05-12 18:32:08 +0700

    createReadTokohPemain

--- tokoh/views.py
+++ tokoh/views.py
@@ -13,11 +13,29 @@ def dictfetchall(cursor):
         for row in cursor.fetchall()
     ]
 
-def read_tokoh(request):
+def read_tokoh_admin(request):
     if request.session['role'] == 'pemain':
         return redirect("/")
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM TOKOH")
         tabel = dictfetchall(cursor)
     context = {'semuatokoh': tabel}
-    return render(request, 'read_tokoh.html', context)
+    return render(request, 'read_tokoh_admin.html', context)
+
+def read_tokoh_pemain(request):
+    if request.session['role'] == 'admin':
+        return redirect("/")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM TOKOH WHERE USERNAME_PENGGUNA = %s;", [request.session['username']])
+        tabel = dictfetchall(cursor)
+    context = {'semuatokohpemain': tabel}
+    return render(request, 'read_tokoh_pemain.html', context)    
+
+def action_read_tokoh(request):
+    if request.session['role'] == 'pemain':
+        return redirect("/")
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT NAMA, ID_MATA, ID_RAMBUT, ID_RUMAH, WARNA_KULIT, PEKERJAAN FROM TOKOH")
+        tabel = dictfetchall(cursor)
+    context = {'semuatokoh': tabel}
+    return render(request, 'action.html', context)
