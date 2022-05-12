078dc6600e6f022fca6fc8679e14dd3dae3530fe
Sulthan Muhammad Fasda <sulthan.muhammad01@ui.ac.id>
2022-05-12 23:59:32 +0700

    create_tokoh

--- tokoh/views.py
+++ tokoh/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.db import connection
+from django.db import connection, IntegrityError
+from .forms import CreateTokoh
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
 
 def dictfetchall(cursor):
     columns = [col[0] for col in cursor.description]
@@ -32,10 +35,55 @@ def read_tokoh_pemain(request):
     return render(request, 'read_tokoh_pemain.html', context)    
 
 def action_read_tokoh(request):
-    if request.session['role'] == 'pemain':
-        return redirect("/")
     with connection.cursor() as cursor:
-        cursor.execute("SELECT NAMA, ID_MATA, ID_RAMBUT, ID_RUMAH, WARNA_KULIT, PEKERJAAN FROM TOKOH")
+        cursor.execute("""  SELECT NAMA, ID_MATA, ID_RAMBUT, ID_RUMAH, WARNA_KULIT, PEKERJAAN FROM TOKOH 
+                            WHERE   """)
         tabel = dictfetchall(cursor)
     context = {'semuatokoh': tabel}
     return render(request, 'action.html', context)
+
+@csrf_exempt
+def create_tokoh(request):
+    if request.session['role'] == 'admin':
+        messages.add_message(request, messages.WARNING, f"Hanya pemain yang dapat menambahkan tokoh")
+        return redirect("/")
+    if request.method == "POST":
+
+                try:
+                    with connection.cursor() as cursor:
+
+                        cursor.execute(f"""
+                            INSERT INTO TOKOH VALUES 
+                            ('{request.session['username']}',
+                            '{request.POST['nama_tokoh']}',
+                            '{request.POST['jenis_kelamin']}',
+                            'Aktif',
+                            0,
+                            100,
+                            0,
+                            0,
+                            '{request.POST['warna_kulit']}',
+                            1,
+                            'Kreatif',
+                            '{request.POST['pekerjaan']}',
+                            'RB001',
+                            'MT001',
+                            'RM001')
+                        """)
+
+                        messages.add_message(request, messages.SUCCESS, "Data Tokoh Telah Disimpan!")
+
+                        return redirect("/")
+                except IntegrityError:
+                    messages.add_message(request, messages.WARNING, f"Data tokoh dengan nama {request.POST['nama_tokoh']} sudah terdaftar")
+
+    with connection.cursor() as cursor:
+
+        cursor.execute("SELECT KODE FROM WARNA_KULIT")
+        context = {"list_warna_kulit" : cursor.fetchall()}
+
+        cursor.execute("SELECT NAMA FROM PEKERJAAN")
+
+        context["list_pekerjaan"] = cursor.fetchall()
+
+        return render(request, "create_tokoh.html", context)
