1dd336e7b86dda67f714b93922c4479bacfedf34
Sulthan Muhammad Fasda <sulthan.muhammad01@ui.ac.id>
2022-05-13 14:39:41 +0700

    fix_create_apparel

--- menggunakan_apparel/urls.py
+++ menggunakan_apparel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import read_menggunakan_apparel_pemain, create_menggunakan_apparel, read_menggunakan_apparel_admin
+from .views import get_apparel, read_menggunakan_apparel_pemain, create_menggunakan_apparel, read_menggunakan_apparel_admin
 app_name = 'menggunakan_apparel'
 
 urlpatterns = [
 	path('create_menggunakan_apparel/', create_menggunakan_apparel, name='create_menggunakan_apparel'),
     path('read_menggunakan_apparel_pemain/', read_menggunakan_apparel_pemain, name='read_menggunakan_apparel_pemain'),
     path('read_menggunakan_apparel_admin/', read_menggunakan_apparel_admin, name='read_menggunakan_apparel_admin'),
+    path('create_menggunakan_apparel/get_apparel/', get_apparel, name='get_apparel'),
 ]
