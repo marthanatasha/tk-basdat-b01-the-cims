b10fc9c20861b7419cce8362ed1569bd6bb2ee45
Adillah Putri <adllhputri@gmail.com>
2022-05-16 14:54:54 +0000

    STATICFILES_DIRS in settings.py

--- the_cims/settings.py
+++ the_cims/settings.py
@@ -135,11 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
